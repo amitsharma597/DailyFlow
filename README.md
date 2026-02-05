4495711d78f24e1cb286372b24c034542e4c066d
amitsharma597 <amitsharmahsp508@gmail.com>
2026-02-05 20:21:16 +0530

    add javascript logic to add tasks

---
+++ README.md
@@ -0,0 +1,11 @@
+# to-do list web application
+A simple and responsive to-do list web application built using HTML, CSS and Javascript.
+
+## Features
+. Add task
+. Clean and responsive UI
+
+## Technologies used
+. HTML
+. CSS
+. Javascript 
--- jss/app.js
+++ jss/app.js
@@ -1,1 +1,32 @@
 
+document.addEventListener('DOMContentLoaded', ()=> {
+    const taskinput = document.getElementById("task-input");
+    const addtaskbtn = document.getElementById("add-task-btn");
+    const tasklist = document.getElementById("task-list");
+
+    const addtask = (event) => {
+        const tasktext = taskinput.value.trim();
+        if(!tasktext) {
+            return
+        }
+
+        const li = document.createElement("li");
+        
+        li.textContent = tasktext;
+        
+        tasklist.appendChild(li);
+        taskinput.value = '';
+
+    };
+
+    addtaskbtn.addEventListener('click',addtask);
+    taskinput.addEventListener('keypress',(e) =>{
+        if(e.key === 'enter')
+        {
+            addtask(e);
+        }
+
+        })
+
+
+})
