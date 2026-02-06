6bb299c01ac1824f79277c29de913827a76a75ae
amitsharma597 <amitsharmahsp508@gmail.com>
2026-02-06 21:23:52 +0530

    javascript logic to delete task and refine styling

--- jss/app.js
+++ jss/app.js
@@ -4,6 +4,8 @@ document.addEventListener('DOMContentLoaded', ()=> {
     const addtaskbtn = document.getElementById("add-task-btn");
     const tasklist = document.getElementById("task-list");
 
+
+
     const addtask = (event) => {
         const tasktext = taskinput.value.trim();
         if(!tasktext) {
@@ -12,16 +14,35 @@ document.addEventListener('DOMContentLoaded', ()=> {
 
         const li = document.createElement("li");
         
-        li.textContent = tasktext;
+        li.innerHTML = `<input type="checkbox" class="checkbox"></input>
+        <span>${tasktext}</span>
+        <div class="task-buttons">
+              <button class="delete-btn">Delete</button>
+        </div>
+
+        
+
+           
+       
+       `;
+
+       li.querySelector(".delete-btn").addEventListener('click', ()=>{
+        li.remove();
+       });
+
+       
+
         
         tasklist.appendChild(li);
         taskinput.value = '';
 
     };
 
+
+
     addtaskbtn.addEventListener('click',addtask);
     taskinput.addEventListener('keypress',(e) =>{
-        if(e.key === 'enter')
+        if(e.key === 'Enter')
         {
             addtask(e);
         }
